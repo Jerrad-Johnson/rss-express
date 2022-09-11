28d8e490499c4ee6ab9a53fb947fdfb25bec16b9
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-09-11 16:58:42 -0600

    Installed package axios

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,22 @@
+let cc = console.log;
+
 function Home(){
+
+    getXML();
     return (
         <>
             Test
         </>
     )
+}
+
+async function getXML(url = `https://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1`){
+    let request = new XMLHttpRequest();
+    await request.open("GET", url, true);
+    cc(4)
+    request.onreadystatechange = () => {
+        cc(5)
+    }
 }
 
 export default Home;
