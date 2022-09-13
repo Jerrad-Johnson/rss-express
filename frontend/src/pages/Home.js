042eddebd9b65ae29fbacace36915889d765f3cb
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-09-12 23:45:01 -0600

    Data from each feed now renders in its own Card component.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,22 +1,51 @@
+import {useEffect, useState} from "react";
 import axios from "axios";
 let serverURL = 'http://localhost:3001';
 
 let cc = console.log;
 
 function Home(){
+   const [feeds, setFeeds] = useState([{
+       url: "https://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1",
+       position: 0,
+   }, {
+       url: "https://slickdeals.net/newsearch.php?mode=popdeals&searcharea=deals&searchin=first&rss=1",
+       position: 1,
+   }]);
 
-    getXML("https://slickdeals.net/newsearch.php?mode=frontpage&searcharea=deals&searchin=first&rss=1");
+    let feedDOMCards = feeds.map((e) => {
+        return (
+            <RSSCard key={e.position} url={e.url} position={e.position}/>
+        )
+    });
+
     return (
         <>
-            Test
+            {feedDOMCards}
         </>
     )
 }
 
-async function getXML(url){
-    let results = await axios.post("http://localhost:3001/xml/getXML", {feedURL: url});
-    cc(results.data);
+function RSSCard({url, position}){
+    const [rssResults, setRSSResults] = useState("Loading.");
+    const [getEntriesNow, setGetEntriesNow] = useState(true);
 
+    if (getEntriesNow === true) {
+        getXML(url, setRSSResults);
+        setGetEntriesNow(false);
+    }
+
+    return (
+      <>
+          {rssResults !== "Loading." && rssResults.entries[0].title}
+      </>
+    );
+}
+
+async function getXML(url, setRSSEntries){
+    let results = await axios.post("http://localhost:3001/xml/getXML", {feedURL: url});
+    setRSSEntries(results.data);
+    cc(results.data);
 }
 
 export default Home;
