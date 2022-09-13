a2986079a273117ddf0eb274a9c29c65c748c64f
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-09-13 14:07:37 -0600

    Added MUI accordion, and set dark theme.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,14 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
-import {Panel, PanelGroup, Placeholder} from "rsuite";
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
 
 let serverURL = 'http://localhost:3001';
 
@@ -15,6 +23,12 @@ function Home(){
        position: 1,
    }]);
 
+    const theme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
+
     let feedDOMCards = feeds.map((e) => {
         return (
             <RSSCard key={e.position} url={e.url} position={e.position}/>
@@ -22,15 +36,59 @@ function Home(){
     });
 
     return (
-        <>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
             {feedDOMCards}
-        </>
+        </ThemeProvider>
     )
 }
 
 function RSSCard({url, position}){
     const [rssResults, setRSSResults] = useState("Loading.");
     const [getEntriesNow, setGetEntriesNow] = useState(true);
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange =
+        (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+            setExpanded(isExpanded ? panel : false);
+        };
+
+
+    const Accordion = styled((props) => (
+        <MuiAccordion disableGutters elevation={0} square {...props} />
+    ))(({ theme }) => ({
+        border: `1px solid ${theme.palette.divider}`,
+        '&:not(:last-child)': {
+            borderBottom: 0,
+        },
+        '&:before': {
+            display: 'none',
+        },
+    }));
+
+    const AccordionSummary = styled((props) => (
+        <MuiAccordionSummary
+            expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+            {...props}
+        />
+    ))(({ theme }) => ({
+        backgroundColor:
+            theme.palette.mode === 'dark'
+                ? 'rgba(255, 255, 255, .05)'
+                : 'rgba(0, 0, 0, .03)',
+        flexDirection: 'row-reverse',
+        '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+            transform: 'rotate(90deg)',
+        },
+        '& .MuiAccordionSummary-content': {
+            marginLeft: theme.spacing(1),
+        },
+    }));
+
+    const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+        padding: theme.spacing(2),
+        borderTop: '1px solid rgba(0, 0, 0, .125)',
+    }));
 
     if (getEntriesNow === true) {
         setRSSResults("Loading.");
@@ -42,11 +100,19 @@ function RSSCard({url, position}){
 
     if (Array.isArray(rssResults.entries) && rssResults.entries.length > 0){
         panelsFromRSSTitles = rssResults.entries.map((e, k) => {
-            cc(e)
             return (
-                <Panel header={e.title} eventKey={k} className={"panel"} id={`panel${k}`}>
-                    <Placeholder.Paragraph> test</Placeholder.Paragraph>
-                </Panel>
+                    <Accordion expanded={expanded === `panel${k}`} onChange={handleChange(`panel${k}`)}>
+                        <AccordionSummary
+                            aria-controls={`panel${k}bh-content`}
+                            id={`panel${k}bh-header`}
+                        >
+                            <span className={"rssAccordionTitle"}> {e.title} </span>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                                <span className={"rssItemDescription"}>{e.description}</span>
+                                <span className={"rssDateDescription"}>{e.pubDate}</span>
+                        </AccordionDetails>
+                    </Accordion>
             );
         });
     }
@@ -54,24 +120,7 @@ function RSSCard({url, position}){
     return (
       <>
         {rssResults === "Loading." && "LOADING"}
-        {/*{rssResults !== "Loading." && rssResults.entries[0].title}*/}
-
-          <PanelGroup accordion defaultActiveKey={2} bordered>
-              <Panel header="Panel 1" eventKey={1} id="panel1">
-                  <Placeholder.Paragraph />
-              </Panel>
-              <Panel header="Panel 2" eventKey={2} id="panel2">
-                  <Placeholder.Paragraph />
-              </Panel>
-              <Panel header="Panel 3" eventKey={3} id="panel3">
-                  <Placeholder.Paragraph />
-              </Panel>
-          </PanelGroup>
-
-          <PanelGroup accordion defaultActiveKey={1} bordered className={"panelContainer"}>
-              {panelsFromRSSTitles}
-          </PanelGroup>
-
+        {rssResults !== "Loading." && panelsFromRSSTitles}
 
 
 
@@ -85,7 +134,6 @@ function RSSCard({url, position}){
 async function getXML(url, setRSSResults){
     let results = await axios.post("http://localhost:3001/xml/getXML", {feedURL: url});
     setRSSResults(results.data);
-    cc(results.data);
 }
 
 export default Home;
