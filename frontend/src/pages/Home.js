5e85b7048554ef1af8c113d2065c7937d56b9b9b
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-09-13 19:16:10 -0600

    About to finish swapping drawers.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,20 +6,27 @@ import MuiAccordion from '@mui/material/Accordion';
 import MuiAccordionSummary from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import {Container} from "@mui/material";
-import {Typography} from "@mui/material";
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
+import {TextField} from "@mui/material";
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
+import MenuIcon from '@mui/icons-material/Menu';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import MuiAppBar from '@mui/material/AppBar';
+import {IconButton} from "@mui/material";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
 
 let serverURL = 'http://localhost:3001';
 
@@ -59,7 +66,6 @@ function Home(){
         switch(action.type){
             case "setRSSEntriesLimit":
                 return {...state, rssEntriesLimit: action.payload};
-
         }
     }
 
@@ -88,16 +94,27 @@ function Home(){
             onKeyDown={toggleDrawer(anchor, false)}
         >
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
+
+                <ListItem >
+                    <div className={"menuItem"}>
+                        <span>Max Results</span>
+                        <TextField id="standard-basic" type={"number"} value={options.rssEntriesLimit} variant="standard"
+                                   sx={{width: "100%"}} onChange={(e) => {
+                                       cc(e.target.value)
+                                optionsDispatch({type: "setRSSEntriesLimit", payload: +e.target.value})
+                            }}/>
+                    </div>
+                </ListItem>
+
+                {/*                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"test"} />
+                    </ListItemButton>
+                </ListItem>*/}
+
             </List>
             <Divider />
             <List>
@@ -115,20 +132,153 @@ function Home(){
         </Box>
     );
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const drawerWidth = 200;
+
+    const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+        ({ theme, open }) => ({
+            flexGrow: 1,
+            padding: theme.spacing(3),
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: `-${drawerWidth}px`,
+            ...(open && {
+                transition: theme.transitions.create('margin', {
+                    easing: theme.transitions.easing.easeOut,
+                    duration: theme.transitions.duration.enteringScreen,
+                }),
+                marginLeft: 0,
+            }),
+        }),
+    );
+
+    const AppBar = styled(MuiAppBar, {
+        shouldForwardProp: (prop) => prop !== 'open',
+    })(({ theme, open }) => ({
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        ...(open && {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: `${drawerWidth}px`,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        }),
+    }));
+
+    const DrawerHeader = styled('div')(({ theme }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    }));
+
+        const [open, setOpen] = useState(false);
+
+        const handleDrawerOpen = () => {
+            setOpen(true);
+        };
+
+        const handleDrawerClose = () => {
+            setOpen(false);
+        };
+
+
     const drawer = ['left'].map((anchor) => (
         <>
-            <div key={anchor} className={"menuButtonContainer"}>
+            {/*<div key={anchor} className={"menuButtonContainer"}>
                 <Box textAlign={"center"}>
                     <Button onClick={toggleDrawer(anchor, true)}>Options</Button>
                 </Box>
-            </div>
+            </div>*/}
+
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={{ mr: 2 }}
+            >Options</IconButton>
+
             <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+
+
+
+
+
+
+
+{/*            <Drawer
+                variant={"persistent"}
                 anchor={anchor}
                 open={drawerState[anchor]}
                 onClose={toggleDrawer(anchor, false)}
             >
                 {list(anchor)}
-            </Drawer>
+            </Drawer>*/}
         </>
     ));
 
@@ -140,6 +290,7 @@ function Home(){
                     {feedDOMCards}
                 </div>
             {drawer}
+
             </div>
 
         </ThemeProvider>
@@ -240,7 +391,6 @@ function RSSCard({url, position, options, optionsDispatch}){
       <>
       <div className={"rssCardContainer"}>
           <>
-
             {feedTitle}
             {rssResults === "Loading." && "LOADING"}
             {rssResults !== "Loading." && panelsFromRSSTitles}
