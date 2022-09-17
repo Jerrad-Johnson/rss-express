6eea2985d3c60089e05027920359351c4a94752f
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-09-17 17:34:03 -0600

    Added to login page.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,11 @@
+import {Box, Card, Container, Paper} from "@mui/material";
+
+
 export default function Login(){
     return (
-        <>
-        </>
+        <div className={"loginContainer"}>
+
+                Test
+        </div>
     )
 }
