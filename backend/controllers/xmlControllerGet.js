cd332859f3ad871ce5614f3e44ffe1ed8cbff580
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-09-17 16:00:17 -0600

    Refactored. Better for unit testing.

--- backend/controllers/xmlControllerGet.js
+++ backend/controllers/xmlControllerGet.js
@@ -1,9 +1,10 @@
 const {getXML} = require("../models/getXML");
 const {parseXML} = require("../models/parseXML");
+const {standardizedResponse} = require("../utils/fns");
 const cc = console.log;
-
 
 exports.xmlControllerGet = async (req, res) => {
     let feedResponse = await getXML(req, res);
-    if (feedResponse?.data) parseXML(req, res, feedResponse);
+    let newResObject = parseXML(req, res, feedResponse);
+    newResObject.send(standardizedResponse("", newResObject.locals));
 }
--- backend/models/parseXML.js
+++ backend/models/parseXML.js
@@ -1,11 +1,13 @@
 const {parser, standardizedResponse} = require("../utils/fns");
 
 exports.parseXML = (req, res, feedResponse) => {
-    let parsedResponse = parser.parse(feedResponse.data);
-    res.locals.entries = parsedResponse.rss.channel.item;
-    res.locals.feedTitle = parsedResponse.rss.channel.title;
-    res.locals.feedLink = req.body.feedURL;
-    if (parsedResponse?.rss?.channel?.lastBuildDate) res.locals.lastUpdated = parsedResponse.rss.channel.lastBuildDate;
+    let newResObject = structuredClone(res);
 
-    res.send(standardizedResponse("", res.locals));
+    let parsedResponse = parser.parse(feedResponse.data);
+    newResObject.locals.entries = parsedResponse.rss.channel.item;
+    newResObject.locals.feedTitle = parsedResponse.rss.channel.title;
+    newResObject.locals.feedLink = req.body.feedURL;
+    if (parsedResponse?.rss?.channel?.lastBuildDate) newResObject.locals.lastUpdated = parsedResponse.rss.channel.lastBuildDate;
+
+    return newResObject;
 }
